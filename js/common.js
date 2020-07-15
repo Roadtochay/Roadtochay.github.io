dfae6e1fd7d7821630e9e553653ca31182b80651
Roadtochay <65916724+Roadtochay@users.noreply.github.com>
2020-07-15 17:49:54 +0300

    Create common.js

---
+++ js/common.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+
+
+
+	 $(".popup").magnificPopup({type:"image"});
+	 $(".popup_c").magnificPopup();
+
+	$.stellar({
+		responsive: true,
+		horizontalOffset: 50
+	});
+
+	$(".owl-carousel").owlCarousel({
+		responsive : {
+			0 : {
+				items : 1,
+				nav : true
+			}
+		},
+		navText : ""
+	});
+
+
+
+	function wResize() {
+		$("header").css("min-height", $(window).height());
+	};
+	wResize();
+	$(window).resize(function() {
+		wResize()
+	});
+
+
+	$(".top_phone .tab_item").not(":first").hide();
+	$(".top_phone .wrapper .tab").click(function() {
+		$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$(".tabs_header .tab_item").not(":first").hide();
+	$(".tabs_header .wrapper .tab").click(function() {
+		$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$(".contacts_top .tab_item").not(":first").hide();
+	$(".contacts_top  .tab").click(function() {
+		$(".contacts_top .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".contacts_top .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$(".bottom_phone .tab_item").not(":first").hide();
+	$(".bottom_phone .wrapper .tab").click(function() {
+		$(".bottom_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".bottom_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+
+	
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#form").submit(function(e) {
+		var ths = $(this);
+		e.preventDefault;
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				var magnificPopup = $.magnificPopup.instance; 
+				// save instance in magnificPopup variable
+				magnificPopup.close();
+				ths.trigger("reset"); 
+				// Close popup that is currently opened
+			}, 1000);
+		});
+		return false;
+	});
+	
+});
+
+$(window).load(function() {
+	$(".top_header h1").animated("fadeInDown", "fadeOut");
+	$(".top_header h2").animated("fadeInUp", "fadeOut");
+	$(".tabs_header .wrapper").animated("flipInY", "fadeOut");
+	$(".s_back h3").animated("fadeIn", "fadeOut");
+	$("footer").animated("fadeIn", "fadeOut");
+});
